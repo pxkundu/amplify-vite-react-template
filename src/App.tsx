e404ee313e0c01952f7515007a4a8b9ade711347
Kundu <parthakundu@inneed.cloud>
2024-08-21 07:31:53 -0500

    map ui reverted

--- src/App.tsx
+++ src/App.tsx
@@ -1,109 +1,96 @@
 import { useEffect, useState } from "react";
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import { createMap, drawPoints } from 'maplibre-gl-js-amplify';
-import 'maplibre-gl/dist/maplibre-gl.css';
-import { generateClient } from "aws-amplify/data";
 import type { Schema } from "../amplify/data/resource";
-import type { Map } from 'maplibre-gl'; // Import Map type
+import { generateClient } from "aws-amplify/data";
+
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
+// import { MapView } from "@aws-amplify/ui-react-geo";
+// import { NavigationControl } from "react-map-gl";
+
+// import '@aws-amplify/ui-react-geo/styles.css';
+
+import { createMap } from 'maplibre-gl-js-amplify';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const [map, setMap] = useState<Map | null>(null); // Initialize map state with correct type
+
+  // const coordinates = {
+  //   longitude: -115.17077150978058,
+  //   latitude: 36.12309017212961,
+  // };
 
   async function initializeMap() {
-    const mapInstance = await createMap({
-      container: 'map', // An HTML Element or HTML element ID to render the map in
+    await createMap({
+      container: 'map', // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js/docs/API/classes/Map/
       center: [-123.1187, 49.2819], // [Longitude, Latitude]
       zoom: 11
     });
-
-    mapInstance.on('load', function () {
-      drawPoints(
-        'mySourceName', // Arbitrary source name
-        [
-          {
-            coordinates: [-122.483696, 37.833818], // [Longitude, Latitude]
-            title: 'Golden Gate Bridge',
-            address: 'A suspension bridge spanning the Golden Gate'
-          },
-          {
-            coordinates: [-122.477, 37.8105] // [Longitude, Latitude]
-          }
-        ],
-        mapInstance,
-        {
-          showCluster: true,
-          unclusteredOptions: {
-            showMarkerPopup: true
-          },
-          clusterOptions: {
-            showCount: true
-          }
-        }
-      );
-    });
-
-    setMap(mapInstance); // Store the map instance in state
   }
-
+  
+  initializeMap();
+  
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+  
   useEffect(() => {
-    initializeMap();
-
-    const subscription = client.models.Todo.observeQuery().subscribe({
+    client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
-
-    return () => {
-      if (map) {
-        map.remove(); // Clean up the map instance on unmount
-      }
-      subscription.unsubscribe();
-    };
-  }, [map]);
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id });
-  }
+  }, []);
 
   function createTodo() {
-    const content = window.prompt("Todo content");
-    if (content) {
-      client.models.Todo.create({ content });
-    }
+    client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
   return (
+        
     <Authenticator>
       {({ signOut, user }) => (
-        <main>
-          <div id="map"></div>
+      // {({ signOut }) => (
+    <main>
+      <div id="map">
+        {/* <MapView
+          initialViewState={{
+            ...coordinates,
+            zoom: 15,
+          }}
+        >
+          <NavigationControl position={"top-left"} />
+        </MapView> */}
 
-          <h1>{user?.signInDetails?.loginId}'s todos</h1>
-          
-          <button onClick={createTodo}>+ new</button>
-          <ul>
-            {todos.map((todo) => (
-              <li 
-                onClick={() => deleteTodo(todo.id)}
-                key={todo.id}>{todo.content}
-              </li>
-            ))}
-          </ul>
-          <div>
-            🥳 App successfully hosted. Try creating a new todo.
-            <br />
-            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-              Review the next step of this tutorial.
-            </a>
-          </div>
-          <button onClick={signOut}>Sign out</button>
-        </main>
+        {/* // initializeMap(); */}
+      </div>
+      
+
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+       {/* <h1>My todos</h1> */}
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li 
+          onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+      <div>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+          Review next step of this tutorial.
+        </a>
+      </div>
+      <button onClick={signOut}>Sign out</button>
+    </main>
+        
       )}
     </Authenticator>
   );
 }
 
 export default App;
+
