48069a360ee1defff9f3cbf5b777a8bbfe90f21e
Kundu <parthakundu@inneed.cloud>
2024-08-21 14:47:48 -0500

    Map only

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
@@ -16,7 +16,7 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  // const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   // const coordinates = {
   //   longitude: -115.17077150978058,
@@ -31,21 +31,22 @@ function App() {
     });
   }
   
-  initializeMap();
   
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
+  
+  // function deleteTodo(id: string) {
+  //   client.models.Todo.delete({ id })
+  // }
   
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
+    initializeMap();
+    // client.models.Todo.observeQuery().subscribe({
+    //   next: (data) => setTodos([...data.items]),
+    // });
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+  // function createTodo() {
+  //   client.models.Todo.create({ content: window.prompt("Todo content") });
+  // }
 
   return (
         
@@ -53,6 +54,7 @@ function App() {
       {({ signOut, user }) => (
       // {({ signOut }) => (
     <main>
+      <h1>{user?.signInDetails?.loginId}'s PatTracker</h1>
       <div id="map">
         {/* <MapView
           initialViewState={{
@@ -67,23 +69,23 @@ function App() {
       </div>
       
 
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      {/* <h1>{user?.signInDetails?.loginId}'s todos</h1>
        {/* <h1>My todos</h1> */}
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
+      {/* <button onClick={createTodo}>+ new</button>
+      <ul> */}
+        {/* {todos.map((todo) => (
           <li 
           onClick={() => deleteTodo(todo.id)}
           key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
+        ))} */}
+      {/* </ul>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
           Review next step of this tutorial.
         </a>
-      </div>
+      </div> */}
       <button onClick={signOut}>Sign out</button>
     </main>
         
