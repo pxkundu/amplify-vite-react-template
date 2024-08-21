2016d2137b37d30f09ee879531767843ede55279
Kundu <parthakundu@inneed.cloud>
2024-08-20 19:46:07 -0500

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -28,9 +28,11 @@ function App() {
   return (
         
     <Authenticator>
-      {({ signOut }) => (
+      {({ signOut, user }) => (
+      // {({ signOut }) => (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+       {/* <h1>My todos</h1> */}
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
@@ -50,7 +52,7 @@ function App() {
     </main>
         
       )}
-      </Authenticator>
+    </Authenticator>
   );
 }
 
